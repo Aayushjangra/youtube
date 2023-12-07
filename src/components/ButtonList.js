4de6d3cee0552be21759af80117c9402372f6b2c
Aayush Jangra <118102480+Aayushjangra@users.noreply.github.com>
2023-12-07 15:29:27 +0530

    Building Youtube

---
+++ src/components/ButtonList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Button from "./Button";
+
+const ButtonList = () => {
+  return (
+    <div className="flex">
+    <Button name="All" />
+    <Button name="Gaming" />
+    <Button name="Songs" />
+    <Button name="Live" />
+    <Button name="Cricket" />
+    <Button name="Cooking" />
+    <Button name="Baseball" />
+    <Button name="Football" />
+    <Button name="Valentines" />
+    <Button name="Ipl" />
+    <Button name="Romantic" />
+    <Button name="News" />
+    <Button name="Bollywood" />
+  </div>
+  );
+};
+
+export default ButtonList;
